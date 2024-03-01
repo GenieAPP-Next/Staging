033a6afe0b9b3f6e5044c3cd7f60b78ef74f765e
Adri Antori <adriantori11@gmail.com>
2024-03-01 09:39:08 +0700

    refactor: edited as per eslint rule

--- src/models/Users.model.ts
+++ src/models/Users.model.ts
@@ -2,7 +2,7 @@ import { DataTypes } from "sequelize";
 
 import sequelize from "@/server-action/middlewares/dbConnection";
 
-const Users = sequelize.define('Users', {
+const Users = sequelize.define("Users", {
   users_id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
@@ -28,14 +28,4 @@ const Users = sequelize.define('Users', {
   },
 });
 
-// Synchronize the model with the database (create the table if it doesn't exist)
-// Note: You might want to handle this differently in a production environment
-// sequelize.sync()
-//   .then(() => {
-//     console.log('Users table synced');
-//   })
-//   .catch((error: any) => {
-//     console.error('Error syncing Users table:', error);
-//   });
-
-module.exports = Users;
+export default Users;
