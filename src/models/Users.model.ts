6146f7e8290f16c77034beab9e3d92ccdd00245a
Adri Antori <adriantori11@gmail.com>
2024-03-01 08:53:02 +0700

    feat: Users model

---
+++ src/models/Users.model.ts
@@ -0,0 +1,41 @@
+import { DataTypes } from "sequelize";
+
+import sequelize from "@/server-action/middlewares/dbConnection";
+
+const Users = sequelize.define('Users', {
+  Users_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password_hash: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+// Synchronize the model with the database (create the table if it doesn't exist)
+// Note: You might want to handle this differently in a production environment
+// sequelize.sync()
+//   .then(() => {
+//     console.log('Users table synced');
+//   })
+//   .catch((error: any) => {
+//     console.error('Error syncing Users table:', error);
+//   });
+
+module.exports = Users;
