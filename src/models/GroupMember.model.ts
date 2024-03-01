768e747fe7d07dc8b9ade9c471b5f6c5bf177538
Adri Antori <adriantori11@gmail.com>
2024-03-01 09:56:54 +0700

    feat: GroupMember model

---
+++ src/models/GroupMember.model.ts
@@ -0,0 +1,44 @@
+import { DataTypes } from "sequelize";
+
+import sequelize from "@/server-action/middlewares/dbConnection";
+
+import Users from "./Users.model";
+import Groups from "./Groups.model";
+
+const GroupMembers = sequelize.define("group_members", {
+  group_member_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  group_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+GroupMembers.belongsTo(Users, {
+  foreignKey: "user_id",
+  targetKey: "user_id",
+});
+
+GroupMembers.belongsTo(Groups, {
+  foreignKey: "group_id",
+  targetKey: "group_id",
+});
+
+export default GroupMembers;
