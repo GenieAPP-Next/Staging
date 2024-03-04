a3dbd0447db13f07f872b391c7ef87aaab4b7103
Muhammmad Insan Faqqih Fiddin <minsanfaqqih@gmail.com>
2024-03-04 15:59:20 +0700

    feat: Create feature add user

---
+++ src/app/api/addMember/route.ts
@@ -0,0 +1,7 @@
+// // myRoute.ts
+// import { NextRequest, NextResponse } from "next/server";
+// import { Addmember } from "@/server-action/controllers/Addmember.controllers";
+// // import { addMember as memberObject } from "@/server-action/types/addMember.types";
+// export async function GET(req: NextRequest, res: NextResponse) {
+//   return await Addmember(req, res);
+// }
---
+++ src/server-action/types/addMember.types.ts
@@ -0,0 +1,3 @@
+export interface addMember {
+    username: string;
+}
