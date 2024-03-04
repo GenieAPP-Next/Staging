01677644db0f81ccfa6c86dc910b22311e947d5c
Dody Hartanto <muharridody@gmail.com>
2024-03-04 22:35:05 +0800

    fix: AppRouterCacheProvider and ThemeProvider wrap html instead of inside html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from "next";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/theme/theme";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "@/theme/theme";
 import "./globals.css";
 
 export const metadata: Metadata = {
