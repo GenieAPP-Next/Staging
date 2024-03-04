abd200991de554156c95f67caa669419a9dd2320
Dody Hartanto <muharridody@gmail.com>
2024-03-04 21:43:35 +0800

    feat: Add join group page using 4 digit code with route : /group/join

--- src/app/(routes)/group/join/page.tsx
+++ src/app/(routes)/group/join/page.tsx
@@ -2,18 +2,20 @@
 
 import Typography from "@mui/material/Typography";
 import { useTheme } from "@mui/material/styles";
-import { BackNavbar, JoinGroupInput } from "@/components";
+import Navbar from "@/components/Navbar/Navbar";
+import JoinGroupInput from "@/components/JoinGroupInput";
 import styles from "@/app/(routes)/group/join/page.module.css";
 
 const JoinGroup = () => {
   const theme = useTheme();
   const handleJoin = (code: string) => {
     console.log("Join group code : " + code);
+    // insert fetch group code function here
   };
 
   return (
     <>
-      <BackNavbar title={"Join Group"} />
+      <Navbar pageTitle={"Join Group"} />
       <div className={styles.main}>
         <Typography sx={{ fontSize: 24, fontWeight: 500, marginBottom: "32px", color: theme.palette.text.primary }}>
           Enter Group Code
