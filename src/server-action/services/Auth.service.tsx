b4888034cdd6ae0c1e1699330a8e3a53bf6288ba
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2024-03-04 20:12:52 +0700

    Add Login Service

--- src/server-action/services/Auth.service.tsx
+++ src/server-action/services/Auth.service.tsx
@@ -1,42 +1,110 @@
 import Users from "@/models/Users.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import bcrypt from "bcrypt";
-import { RegisterInput } from "../types/Auth.types";
-import { getUserEmail } from "../repository/Auth.repository";
+import jwt from "jsonwebtoken";
+import { RegisterInput, LoginInput } from "../types/Auth.types";
+import { getUserAccount, getUserEmail } from "../repository/Auth.repository";
+import { NextApiResponse } from "next";
 
 const registerService = async ({
-  email,
-  username,
-  password,
+	email,
+	username,
+	password,
 }: RegisterInput) => {
-  try {
-    const existEmail = await getUserEmail(email);
-    if (existEmail) {
-      throw new ErrorHandler({
-        success: false,
-        status: 409,
-        message:
-          "The provided email is already registered. Please use a different email address.",
-      });
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await Users.create({
-      email,
-      username,
-      password_hash: hashedPassword,
-    });
-    return {
-      status: 200,
-      message: "registraion successful",
-      data: newUser,
-    };
-  } catch (error: any) {
-    throw new ErrorHandler({
-      success: false,
-      status: error.status,
-      message: error.message,
-    });
-  }
+	try {
+		const existEmail = await getUserEmail(email);
+		if (existEmail) {
+			throw new ErrorHandler({
+				success: false,
+				status: 409,
+				message:
+					"The provided email is already registered. Please use a different email address.",
+			});
+		}
+		const hashedPassword = await bcrypt.hash(password, 10);
+		const newUser = await Users.create({
+			email,
+			username,
+			password_hash: hashedPassword,
+		});
+		return {
+			status: 200,
+			message: "registration successful",
+			data: newUser,
+		};
+	} catch (error: any) {
+		throw new ErrorHandler({
+			success: false,
+			status: error.status,
+			message: error.message,
+		});
+	}
 };
 
-export { registerService };
+const loginService = async ({ email, password }: LoginInput) => {
+	console.log(email, password);
+	try {
+		const user = await getUserAccount(email);
+		console.log(user?.dataValues.password_hash);
+		const JWT_SIGN = "310324";
+
+		if (user) {
+			const isPasswordCorrect: boolean = await bcrypt.compare(
+				password,
+				user?.dataValues.password_hash
+			);
+
+			if (isPasswordCorrect) {
+				const accessToken: string = jwt.sign(
+					{
+						username: user.dataValues.username,
+						role: user.dataValues.role,
+					},
+					JWT_SIGN,
+					{ expiresIn: "1hr" }
+				);
+
+				const refreshToken: string = jwt.sign(
+					{
+						username: user.dataValues.username,
+						role: user.dataValues.role,
+					},
+					JWT_SIGN,
+					{ expiresIn: "7d" }
+				);
+
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				function setCookies(res: NextApiResponse) {
+					res.setHeader("Set-Cookie", [
+						`accessToken=${accessToken}; HttpOnly; Secure; SameSite=None; Max-Age=${1 * 60 * 60}`,
+						`refreshToken=${refreshToken}; HttpOnly; Secure; Max-Age=${7 * 24 * 60 * 60}`,
+					]);
+				}
+
+				return {
+					status: 200,
+					message: "login successful",
+					data: user,
+				};
+			} else {
+				return {
+					status: 401,
+					message: "Password is incorrect",
+				};
+			}
+		} else {
+			return {
+				status: 401,
+				message: "User Not Found",
+			};
+		}
+	} catch (error: any) {
+		throw new ErrorHandler({
+			success: false,
+			status: error.status || 500,
+			message: error.message || "Internal Server Error",
+		});
+	}
+};
+
+export { registerService, loginService };
