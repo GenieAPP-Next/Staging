cefe6ae51f28e066eff87f8a1a87780fdec9740b
itsaFan <steffansim@gmail.com>
2024-03-08 22:14:10 +0800

    find member API done

--- src/components/AddMember/add-member.tsx
+++ src/components/AddMember/add-member.tsx
@@ -1,40 +1,77 @@
 "use client";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import AddMemberLogistics from "./add-member-logistics";
 import MemberSearchInput from "./search-input";
 import SubmitAddedMembers from "./submit-added-members";
 import classes from "./scss/add-member.module.scss";
 import { Member } from "./add-member-interface";
+import { api } from "@/app/api/constant";
 
-interface Props {
-  memberList: Member[];
-}
+const getMemberByUsername = async (username: string) => {
+  try {
+    const response = await api.get(`/findMember/${username}`);
 
-export default function AddMember({ memberList }: Props) {
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export default function AddMember() {
   const [selectedMembers, setSelectedMembers] = useState<Member[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   console.log(selectedMembers);
 
-  const handleSelectMember = (memberName: string) => {
-    const member = memberList.find((m) => m.name === memberName);
-    if (member && !selectedMembers.find((m) => m.name === member.name)) {
-      setSelectedMembers([...selectedMembers, member]);
+  const fetchSearchResults = useCallback(async (username: string) => {
+    setLoading(true);
+    try {
+      const response = await getMemberByUsername(username);
+      const memberData = response.data.data;
+      console.log("consoledata", memberData);
+
+      setSelectedMembers((currentMembers) => {
+        const isMemberExists = currentMembers.some((member) => member.user_id === memberData.user_id);
+
+        if (!isMemberExists && memberData) {
+          clearErrorMessage();
+          return [...currentMembers, memberData];
+        }
+
+        return currentMembers;
+      });
+    } catch (error: any) {
+      console.error("Failed to fetch Member", error);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      setErrorMessage(error.response?.data?.message || "An unexpected error occurred");
+    } finally {
+      setLoading(false);
     }
+  }, []);
+
+  const handleSelectMember = (inputValue: string) => {
+    void fetchSearchResults(inputValue);
   };
 
   const handleDeleteMember = (memberName: string) => {
-    setSelectedMembers(selectedMembers.filter(member => member.name !== memberName));
+    setSelectedMembers(selectedMembers.filter((member) => member.username !== memberName));
+  };
+
+  const clearErrorMessage = () => {
+    setErrorMessage("");
   };
 
   return (
     <section id="add-members">
       <div className={classes.container}>
         <div className={classes.content} style={{ gap: selectedMembers.length > 0 ? "2rem" : "0rem" }}>
-          <MemberSearchInput memberList={memberList} onSelectMember={handleSelectMember} />
+          <MemberSearchInput onSelectMember={handleSelectMember} isLoading={loading} errorMessage={errorMessage} clearError={clearErrorMessage} />
           <div className={classes.wrapper}>
             <AddMemberLogistics selectedMembers={selectedMembers} onDeleteMember={handleDeleteMember} />
           </div>
         </div>
-        <SubmitAddedMembers />
+        <SubmitAddedMembers addedMembers={selectedMembers} />
       </div>
     </section>
   );
--- src/components/AddMember/search-input.tsx
+++ src/components/AddMember/search-input.tsx
@@ -1,35 +1,73 @@
 "use client";
-import { Autocomplete, TextField } from "@mui/material";
+import { Autocomplete, CircularProgress, TextField } from "@mui/material";
 import classes from "./scss/add-member.module.scss";
-import { Member } from "./add-member-interface";
+import { debounce } from "lodash";
+import { useCallback, useEffect, useState } from "react";
+import { capitalizeFirstLetter } from "../utils/helper-converter";
 
 interface Props {
-  memberList: Member[];
-  onSelectMember: (memberName: string) => void;
+  onSelectMember: (inputValue: string) => void;
+  isLoading: boolean;
+  errorMessage: string;
+  clearError: () => void;
 }
 
-export default function MemberSearchInput({ memberList, onSelectMember }: Props) {
+export default function MemberSearchInput({ onSelectMember, isLoading, errorMessage, clearError }: Props) {
+  const [inputValue, setInputValue] = useState("");
+
+  const debouncedOnSelectMember = useCallback(
+    debounce((value: string) => {
+      onSelectMember(value);
+    }, 1000), // 1000ms delay
+    []
+  );
+
+  useEffect(() => {
+    if (inputValue) {
+      debouncedOnSelectMember(inputValue);
+    } else {
+      clearError();
+    }
+
+    return () => {
+      debouncedOnSelectMember.cancel();
+    };
+  }, [inputValue, debouncedOnSelectMember]);
+
+  const options: [] = [];
+
   return (
     <div className={classes.searchLayout}>
       <Autocomplete
-        onChange={(_, newValue) => {
-          if (newValue) onSelectMember(newValue);
-        }}
         className={classes.searchContainer}
         freeSolo
-        id="add-member-search"
-        disableClearable
-        options={memberList.map((option) => option.name)}
+        options={options}
+        onInputChange={(_, newValue) => {
+          setInputValue(newValue);
+          if (!newValue) {
+            clearError();
+          }
+        }}
         renderInput={(params) => (
           <TextField
+            variant="outlined"
             {...params}
+            error={!!errorMessage}
+            helperText={capitalizeFirstLetter(errorMessage)}
             label="Search..."
             InputProps={{
               ...params.InputProps,
-              type: "search",
+              endAdornment: (
+                <>
+                  {isLoading ? <CircularProgress color="inherit" size={20} /> : null}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
             }}
           />
         )}
+        loading={isLoading}
+        loadingText="Loading..."
       />
     </div>
   );
