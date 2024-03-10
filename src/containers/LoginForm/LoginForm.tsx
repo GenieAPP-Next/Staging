dcb6947299364e4c2c75c8b1fb19ec2ff7614a79
sigitwie <wirsigit@gmail.com>
2024-03-10 13:28:10 +0700

    feat : integrate login with api

--- src/containers/LoginForm/LoginForm.tsx
+++ src/containers/LoginForm/LoginForm.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 "use client";
-
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as Yup from "yup";
@@ -10,11 +11,10 @@ import {
   InputAdornment,
   Typography,
   ThemeProvider,
-  Grid,
+  Button,
+  CircularProgress,
 } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import Button from "@mui/material/Button";
-import CircularProgress from "@mui/material/CircularProgress";
 import Image from "next/image";
 import logo from "../../public/genielogo.png";
 import { useRouter } from "next/navigation";
@@ -23,9 +23,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import theme from "../../theme/theme";
 import Link from "next/link";
 import Swal from "sweetalert2";
+import axios from "axios";
+
+interface LoginFormInput {
+  email: string;
+  password: string;
+}
 
 function LoginForm() {
   const [isSubmitting, setSubmitting] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
 
   const validationSchema = Yup.object({
@@ -39,7 +46,7 @@ function LoginForm() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({
+  } = useForm<LoginFormInput>({
     resolver: async (values) => {
       try {
         await validationSchema.validate(values, { abortEarly: false });
@@ -48,39 +55,63 @@ function LoginForm() {
         if (error instanceof Yup.ValidationError) {
           const formErrors = error.inner.reduce<Record<string, any>>(
             (acc, cur) => {
-              acc[cur.path as any] = { message: cur.message };
+              const path = cur.path ?? "";
+              acc[path] = { message: cur.message };
               return acc;
             },
             {}
           );
-          return { values, errors: formErrors };
-        } else {
-          console.error("An unexpected error occurred:", error);
-          throw new Error("An unexpected error occurred");
+
+          return { values: {}, errors: formErrors };
         }
+        throw error;
       }
     },
   });
 
-  const onSubmit = async (data: any) => {
-    console.log(data);
+  const onSubmit = async (data: LoginFormInput) => {
     setSubmitting(true);
-    // Here you can perform any logic you need, like calling an API
-    setTimeout(() => {
+
+    try {
+      const response = await axios.post("/api/auth/login", data);
+
+      if (response.status === 200 && response.data.success) {
+        const userId = response.data.data.user_id;
+
+        localStorage.setItem("user_id", userId.toString());
+
+        void Swal.fire(
+          "Login success",
+          "You have successfully logged in!",
+          "success"
+        );
+
+        // Redirect to /group
+        router.push("/group");
+      } else {
+        // Handle unsuccessful login
+        void Swal.fire(
+          "Login failed",
+          response.data.message || "Invalid login credentials",
+          "error"
+        );
+      }
+    } catch (error: unknown) {
+      console.error("An unexpected error occurred:", error);
+
+      let errorMessage = "An unexpected error occurred";
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+
+      void Swal.fire("Error", errorMessage, "error");
+    } finally {
       setSubmitting(false);
-      router.push("/");
-    }, 2000);
-    void Swal.fire({
-      icon: "success",
-      title: "Login success",
-      text: "You have successfully logged in!",
-    });
+    }
   };
 
-  const [showPassword, setShowPassword] = useState(false);
-
   const handleClickShowPassword = () => {
-    setShowPassword((show) => !show);
+    setShowPassword(!showPassword);
   };
 
   const handleMouseDownPassword = (
@@ -103,167 +134,117 @@ function LoginForm() {
             <Image src={logo} alt="logo" width={167} height={94} />
           </Box>
           <Typography
+            sx={{ padding: "20px" }}
             variant="h5"
             color="textPrimary"
             gutterBottom
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
             textAlign="center"
           >
-            Effortlessly manage gift.
+            Effortlessly manage gift. Free on Genie.
           </Typography>
-          <Typography
-            variant="h5"
-            color="textPrimary"
-            gutterBottom
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            textAlign="center"
-          >
-            Free on Genie.
-          </Typography>
+
           <Box
             component="form"
             onSubmit={handleSubmit(onSubmit)}
             sx={{
               width: "100%",
               maxWidth: "325px",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
               mt: 2,
               padding: 2,
             }}
           >
-            <Grid item xs={12} sm={10} md="auto" lg={4}>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <TextField
-                  fullWidth
-                  label="Email"
-                  variant="outlined"
-                  margin="normal"
-                  size="small"
-                  InputLabelProps={{
-                    style: { fontSize: "14px" },
-                  }}
-                  {...register("email")}
-                  error={!!errors.email}
-                />
-                <TextField
-                  fullWidth
-                  label="Password"
-                  type={showPassword ? "text" : "password"}
-                  variant="outlined"
-                  margin="normal"
-                  size="small"
-                  InputLabelProps={{
-                    style: { fontSize: "14px" },
-                  }}
-                  {...register("password")}
-                  error={!!errors.password}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                          edge="end"
-                        >
-                          {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-                <Button
-                  type="submit"
-                  color="primary"
-                  variant="contained"
-                  disabled={isSubmitting}
-                  style={{
-                    width: "100%",
-                    borderRadius: 15,
-                    marginTop: 25,
-                    fontSize: 12,
-                  }}
-                >
-                  {isSubmitting ? <CircularProgress /> : "Login"}
-                </Button>
-              </form>
-              <Box
-                mt={2}
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Typography variant="body2" color="grey">
-                  Forgot your login details?
-                  <Link href="/reset-password">
-                    <strong> Get help logging in.</strong>
-                  </Link>
-                </Typography>
-              </Box>
-              <Box
-                mt={3}
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <Box
-                  borderBottom="0.09999999999999964pt solid #c3c3c3"
-                  width="100%"
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                ></Box>
-                <Typography
-                  variant="subtitle2"
-                  color="grey"
-                  alignItems="center"
-                  style={{ padding: "0 25px" }}
-                >
-                  Or
-                </Typography>
-                <Box
-                  borderBottom="0.09999999999999964pt solid #c3c3c3"
-                  width="100%"
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                ></Box>
-              </Box>
-              <Box mt={2} display="flex" justifyContent="center">
-                <IconButton>
-                  <FontAwesomeIcon icon={faGoogle} color="black" size="1x" />
-                </IconButton>
-                <IconButton>
-                  <FontAwesomeIcon icon={faFacebookF} color="black" size="1x" />
-                </IconButton>
-              </Box>
-              <Box
-                mt={20}
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                justifyContent="center"
-              >
-                <Typography
-                  variant="body2"
-                  color="grey"
-                  justifyContent="center"
-                  alignItems="center"
-                >
-                  Don't have an account?{" "}
-                  <Link href="/register">
-                    <strong>Sign up.</strong>
-                  </Link>
-                </Typography>
-              </Box>
-            </Grid>
+            <TextField
+              fullWidth
+              label="Email"
+              variant="outlined"
+              margin="normal"
+              size="small"
+              InputLabelProps={{ style: { fontSize: "14px" } }}
+              {...register("email")}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+            />
+            <TextField
+              fullWidth
+              label="Password"
+              type={showPassword ? "text" : "password"}
+              variant="outlined"
+              margin="normal"
+              size="small"
+              InputLabelProps={{ style: { fontSize: "14px" } }}
+              {...register("password")}
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <Button
+              type="submit"
+              color="primary"
+              variant="contained"
+              disabled={isSubmitting}
+              sx={{
+                width: "100%",
+                borderRadius: 15,
+                mt: 3,
+                fontSize: 12,
+              }}
+            >
+              {isSubmitting ? <CircularProgress size={24} /> : "Login"}
+            </Button>
+          </Box>
+
+          <Box mt={2}>
+            <Typography variant="body2" color="grey" textAlign="center">
+              Forgot your login details?
+              <Link href="/reset-password">
+                <strong> Get help logging in.</strong>
+              </Link>
+            </Typography>
+          </Box>
+
+          <Box
+            mt={3}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Typography
+              variant="subtitle2"
+              color="grey"
+              sx={{ padding: "0 25px" }}
+            >
+              Or
+            </Typography>
+          </Box>
+
+          <Box mt={2} display="flex" justifyContent="center">
+            <IconButton>
+              <FontAwesomeIcon icon={faGoogle} color="black" size="1x" />
+            </IconButton>
+            <IconButton>
+              <FontAwesomeIcon icon={faFacebookF} color="black" size="1x" />
+            </IconButton>
+          </Box>
+
+          <Box mt={20}>
+            <Typography variant="body2" color="grey" textAlign="center">
+              Don't have an account?{" "}
+              <Link href="/register">
+                <strong>Sign up.</strong>
+              </Link>
+            </Typography>
           </Box>
         </Box>
       </main>
