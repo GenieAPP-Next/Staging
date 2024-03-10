c85bcea99db99585224ee8d0d3c7ab755f68f289
Ireng Febrian <131000271+febriaan22@users.noreply.github.com>
2024-03-10 19:09:50 +0700

    Feat: Add Gift Types

---
+++ src/server-action/types/addGift-Ireng.types.ts
@@ -0,0 +1,11 @@
+interface addGiftTypes {
+    groupId: number
+    name: string,
+    price:  number
+    imageUrl: string
+    urlLink: string
+    userId: number
+    categoryId: number
+}
+
+export default addGiftTypes
