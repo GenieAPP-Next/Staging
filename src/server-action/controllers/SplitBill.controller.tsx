64b249b6cf512317228e248c0dac54f1ccfd46e2
adriantori <adriantori11@gmail.com>
2024-03-11 09:16:43 +0700

    feat: splitBil controller

--- src/server-action/controllers/SplitBill.controller.tsx
+++ src/server-action/controllers/SplitBill.controller.tsx
@@ -1,26 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const splitBill = async (req: NextRequest, res: NextResponse) => {
-    try {
-        const body = await req.json();
-        const { totalAmount } = body;
+export const splitBill = async (req: NextRequest, res: NextResponse) => {
+    const body = await req.text(); // Use text() instead of json()
+    const parsedBody = body ? JSON.parse(body) : {};
+    console.log(parsedBody);
+    return NextResponse.json(
+        {
+            success: true,
+            message: parsedBody,
+        },
+        { status: 200 }
+    );
 
-        return NextResponse.json(
-            {
-                success: true,
-                message: body,
-            },
-            { status: 200 }
-        );
-    } catch (error: any) {
-        return NextResponse.json(
-            {
-                success: false,  // Change success to false in case of an error
-                message: error.message,
-            },
-            { status: 400 }  // Change status to 400 for a bad request
-        );
-    }
+    // try {
+    //     const { email, username, password } = body;
+
+
+    //     return NextResponse.json(
+    //         {
+    //             success: true,
+    //             message: body,
+    //         },
+    //         { status: 200 }
+    //     );
+    // } catch (error: any) {
+    //     return NextResponse.json(
+    //         {
+    //             success: false,  // Change success to false in case of an error
+    //             message: error.message,
+    //         },
+    //         { status: 400 }  // Change status to 400 for a bad request
+    //     );
+    // }
 }
-
-export { splitBill };
