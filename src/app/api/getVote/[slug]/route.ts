09d6e7548000dd69d89c0cf22096b701dc4edc00
sigitwie <wirsigit@gmail.com>
2024-03-12 17:55:42 +0700

    fix: add missing line

--- src/app/api/getVote/[slug]/route.ts
+++ src/app/api/getVote/[slug]/route.ts
@@ -1,5 +1,4 @@
 import { Getvote } from "@/server-action/controllers/Getvote.controller";
 export async function GET(request: any, { params }: any) {
-  return  Getvote(Number(params.slug));
+  return await Getvote(Number(params.slug));
 }
-
