96bafa3dd9294be7b744be97d69489de9bb04603
sigitwie <wirsigit@gmail.com>
2024-03-13 15:36:48 +0700

    feat : add loading state

--- src/components/Split-Bill/SplitBill.tsx
+++ src/components/Split-Bill/SplitBill.tsx
@@ -3,6 +3,7 @@
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import CircularProgress from "@mui/material/CircularProgress";
 import BillPayerLogistics from "./bill-payer-logistics";
 import GiftItemLogistics from "./gift-items-logistics";
 import classes from "./scss/split-bill.module.scss";
@@ -12,6 +13,7 @@ import { SplitBillData } from "./type";
 
 export default function SplitBill() {
   const [data, setData] = useState<SplitBillData | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
   const groupId = 31;
   const userId = 7;
 
@@ -21,26 +23,33 @@ export default function SplitBill() {
         const response = await axios.get(
           `/api/getSplitBill/${groupId}/${userId}`
         );
-        // console.log(response.data);
         setData(response.data.data.data);
       } catch (error) {
         console.error("Error fetching data: ", error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     void fetchData();
   }, [groupId, userId]);
 
-  // Assuming the original amount due per member is in the Bill object
   const originalAmountDue = data?.Bill?.Bill?.total_amount
     ? parseFloat(data.Bill.Bill.total_amount)
     : 0;
 
-  // Extracting the required data
   const billPayerId = data?.Bill?.BillPayer ?? 0;
   const totalAmount = data?.Bill?.Bill?.total_amount
     ? parseFloat(data.Bill.Bill.total_amount)
     : 0;
+
+  if (isLoading) {
+    return (
+      <div className={classes.loadingContainer}>
+        <CircularProgress />
+      </div>
+    );
+  }
 
   return (
     <section id="split-bill">
@@ -49,7 +58,7 @@ export default function SplitBill() {
           {data?.Gift && <GiftItemLogistics gift={data.Gift} />}
           {data?.Bill?.BillPayer && (
             <BillPayerLogistics
-              billPayerName={""}
+              billPayerName={"Kang Bayar"} // replace with actual name
               billPayerId={billPayerId}
               totalAmount={totalAmount}
             />
