9c631bfbddc4b7fb1492f70a17bdba3d11da4f88
sigitwie <wirsigit@gmail.com>
2024-03-13 22:22:36 +0700

    fix : error data type

--- src/app/(routes)/[groupName]/gift/items.ts
+++ src/app/(routes)/[groupName]/gift/items.ts
@@ -4,23 +4,27 @@ export const items = [
     itemName: "Nike Air Max",
     price: 1900000,
     itemImage: "/img/img01.png",
+    creator: "Creator Name",
   },
   {
     id: "2",
     itemName: "Daniel Wellington",
     price: 2345000,
     itemImage: "/img/img02.png",
+    creator: "Creator Name",
   },
   {
     id: "3",
     itemName: "Calvin Klein Handbag",
     price: 4500000,
     itemImage: "/img/img03.png",
+    creator: "Creator Name",
   },
   {
     id: "4",
     itemName: "Hush Puppies Flat Shoes",
     price: 1999000,
     itemImage: "/img/img04.png",
+    creator: "Creator Name",
   },
 ];
